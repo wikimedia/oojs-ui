cb537a24719bc1ca31eacc5d124e9e0cfca4790a
James D. Forrester <jforrester@wikimedia.org>
2014-01-05 19:55:17 +0800

    Update build headers for 2014
    Change-Id: I248154f8cae6543861172822cda8b10b9bbe0e15

--- src/styles/intro.css
+++ src/styles/intro.css
@@ -2,7 +2,7 @@
  * OOjs UI v@VERSION
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
- * Copyright 2011-2013 OOjs Team and other contributors.
+ * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
