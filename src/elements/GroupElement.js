d492a0f6cf1244b62e5ac03620d9f663e8adc5fc
Roan Kattouw <roan.kattouw@gmail.com>
2014-04-21 12:36:08 -0700

    Revert "Add aggregate method"
    None of the callers to the old aggregations mechanism were updated.
    
    This reverts commit b1c75fa83af438facb070bc5138a262ae4436a10.
    
    Bug: 64194
    Change-Id: Iea1b638c19e936239e72f4bf6f0a18c4658af7c6

--- src/elements/GroupElement.js
+++ src/elements/GroupElement.js
@@ -7,6 +7,7 @@
  * @constructor
  * @param {jQuery} $group Container node, assigned to #$group
  * @param {Object} [config] Configuration options
+ * @cfg {Object.<string,string>} [aggregations] Events to aggregate, keyed by item event name
  */
 OO.ui.GroupElement = function OoUiGroupElement( $group, config ) {
 	// Configuration
@@ -16,7 +17,8 @@ OO.ui.GroupElement = function OoUiGroupElement( $group, config ) {
 	this.$group = $group;
 	this.items = [];
 	this.$items = this.$( [] );
-	this.aggregateItemEvents = {};
+	this.aggregate = !$.isEmptyObject( config.aggregations );
+	this.aggregations = config.aggregations || {};
 };
 
 /* Methods */
@@ -28,54 +30,6 @@ OO.ui.GroupElement = function OoUiGroupElement( $group, config ) {
  */
 OO.ui.GroupElement.prototype.getItems = function () {
 	return this.items.slice( 0 );
-};
-
-/**
- * Add an aggregate item event.
- *
- * Aggregated events are listened to on each item and then emitted by the group under a new name,
- * and with an additional leading parameter containing the item that emitted the original event.
- * Other arguments that were emitted from the original event are passed through.
- *
- * @param {Object.<string,string|null>} events Aggregate events emitted by group, keyed by item
- *   event, use null value to remove aggregation
- * @throws {Error} If aggregation already exists
- */
-OO.ui.GroupElement.prototype.aggregate = function ( events ) {
-	var i, len, item, add, remove, itemEvent, groupEvent;
-
-	for ( itemEvent in events ) {
-		groupEvent = events[itemEvent];
-
-		// Remove existing aggregated event
-		if ( itemEvent in this.aggregateItemEvents ) {
-			// Don't allow duplicate aggregations
-			if ( groupEvent ) {
-				throw new Error( 'Duplicate item event aggregation for ' + itemEvent );
-			}
-			// Remove event aggregation from existing items
-			remove = {};
-			remove[itemEvent] = this.aggregateItemEvents[itemEvent];
-			for ( i = 0, len = this.items.length; i < len; i++ ) {
-				this.items[i].disconnect( this, remove );
-			}
-			// Prevent future items from aggregating event
-			delete this.aggregateItemEvents[itemEvent];
-		}
-
-		// Add new aggregate event
-		if ( groupEvent ) {
-			// Make future items aggregate event
-			this.aggregateItemEvents[itemEvent] = groupEvent;
-			// Add event aggregation to existing items
-			for ( i = 0, len = this.items.length; i < len; i++ ) {
-				item = this.items[i];
-				add = {};
-				add[itemEvent] = [ 'emit', groupEvent, item ];
-				item.connect( this, add );
-			}
-		}
-	}
 };
 
 /**
@@ -102,10 +56,10 @@ OO.ui.GroupElement.prototype.addItems = function ( items, index ) {
 			}
 		}
 		// Add the item
-		if ( !$.isEmptyObject( this.aggregateItemEvents ) ) {
+		if ( this.aggregate ) {
 			events = {};
-			for ( event in this.aggregateItemEvents ) {
-				events[event] = [ 'emit', this.aggregateItemEvents[event], item ];
+			for ( event in this.aggregations ) {
+				events[event] = [ 'emit', this.aggregations[event], item ];
 			}
 			item.connect( this, events );
 		}
@@ -145,7 +99,7 @@ OO.ui.GroupElement.prototype.removeItems = function ( items ) {
 		item = items[i];
 		index = $.inArray( item, this.items );
 		if ( index !== -1 ) {
-			if ( !$.isEmptyObject( this.aggregateItemEvents ) ) {
+			if ( this.aggregate ) {
 				item.disconnect( this );
 			}
 			item.setElementGroup( null );
@@ -171,7 +125,7 @@ OO.ui.GroupElement.prototype.clearItems = function () {
 	// Remove all items
 	for ( i = 0, len = this.items.length; i < len; i++ ) {
 		item = this.items[i];
-		if ( !$.isEmptyObject( this.aggregateItemEvents ) ) {
+		if ( this.aggregate ) {
 			item.disconnect( this );
 		}
 		item.setElementGroup( null );
