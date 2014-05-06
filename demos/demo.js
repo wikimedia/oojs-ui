wikimedia/oojs-ui

commit 424b40373e4731585cd99adccfd765c869256755

FieldLayout, styling improvements and standardization
FieldLayout
- Added FieldLayout which provides label and layout features to a widget
- Made Fieldset support adding FieldLayout items
- Removed CheckboxWidget (not needed now, just place a checkbox in a
  FieldLayout and set 'align' to 'inline'
- Updated demo to use FieldLayout
- Moved input cleverness of InputLabelWidget to FieldLayout, and renamed
  it to LabelWidget

Styling improvements
- Made ButtonGroup and ButtonSelect widgets never wrap
- Made Fieldsets use IconedElement mixin instead of rolling its own icon
  support

Standardization
- Added support for passing 'items' config option to most widgets that
  mixin GroupElement (not all should have this, so it's not done at the
  top level)
- Updated all uses of mixins to include final config argument allowing
  config options to be added to mixin classes without updating all callers
- Added config option to clippable element for future use
- Renamed 'options' to 'config' in Toolbar

Change-Id: I87e1bdf5ccb67ce58022926c4faede0c70a1f4a4

demos/widgets.js
@@ -1,237 +1,234 @@
 $( function () {
-	var i, name, len, item, items, fieldset,
+	var name, fieldset,
 		$demo = $( '.oo-ui-demo' ),
 		sections = {
 			'Widgets': [
-				{
-					'label': 'ButtonWidget',
-					'widget': new OO.ui.ButtonWidget( { 'label': 'Normal' } )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( { 'label': 'Normal' } ),
+					{ 'label': 'ButtonWidget (normal)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Primary',
 						'flags': [ 'primary' ]
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (primary)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Constructive',
 						'flags': [ 'constructive' ]
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (constructive)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Destructive',
 						'flags': [ 'destructive' ]
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (destructive)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Disabled',
 						'disabled': true
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (disabled)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Iconed',
 						'icon': 'check'
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (iconed)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'label': 'Indicated',
 						'indicator': 'down'
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (indicated)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'frameless': true,
 						'icon': 'help',
 						'title': 'Icon only'
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (icon only)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'frameless': true,
 						'icon': 'alert',
 						'label': 'Labeled'
-					} )
-				},
-				{
-					'widget': new OO.ui.ButtonWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (frameless)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonWidget( {
 						'frameless': true,
 						'icon': 'menu',
 						'label': 'Disabled',
 						'disabled': true
-					} )
-				},
-				{
-					'label': 'ButtonGroupWidget',
-					'widget': new OO.ui.ButtonGroupWidget(),
-					'items': [
-						new OO.ui.ButtonWidget( { 'icon': 'check', 'indicator': 'down' } ),
-						new OO.ui.ToggleButtonWidget( {
-							'label': 'Primary',
-							'flags': [ 'primary' ]
-						} ),
-						new OO.ui.ToggleButtonWidget( {
-							'label': 'Constructive',
-							'flags': [ 'constructive' ]
-						} ),
-						new OO.ui.ToggleButtonWidget( {
-							'label': 'Destructive',
-							'flags': [ 'destructive' ]
-						} )
-					]
-				},
-				{
-					'label': 'ButtonSelectWidget',
-					'widget': new OO.ui.ButtonSelectWidget(),
-					'items': [
-						new OO.ui.ButtonOptionWidget( 'a',  { 'icon': 'check', 'indicator': 'down' } ),
-						new OO.ui.ButtonOptionWidget( 'b', {
-							'label': 'Primary',
-							'flags': [ 'primary' ]
-						} ),
-						new OO.ui.ButtonOptionWidget( 'c', {
-							'label': 'Constructive',
-							'flags': [ 'constructive' ]
-						} ),
-						new OO.ui.ButtonOptionWidget( 'd', {
-							'label': 'Destructive',
-							'flags': [ 'destructive' ]
-						} )
-					]
-				},
-				{
-					'widget': new OO.ui.ButtonSelectWidget( {
+					} ),
+					{ 'label': 'ButtonWidget (frameless, disabled)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonGroupWidget( {
+						'items': [
+							new OO.ui.ButtonWidget( { 'icon': 'check', 'indicator': 'down' } ),
+							new OO.ui.ToggleButtonWidget( {
+								'label': 'Primary',
+								'flags': [ 'primary' ]
+							} ),
+							new OO.ui.ToggleButtonWidget( {
+								'label': 'Constructive',
+								'flags': [ 'constructive' ]
+							} ),
+							new OO.ui.ToggleButtonWidget( {
+								'label': 'Destructive',
+								'flags': [ 'destructive' ]
+							} )
+						]
+					} ),
+					{ 'label': 'ButtonGroupWidget' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonSelectWidget( {
+						'items': [
+							new OO.ui.ButtonOptionWidget( 'a',  {
+								'icon': 'check', 'indicator': 'down'
+							} ),
+							new OO.ui.ButtonOptionWidget( 'b', {
+								'label': 'Primary',
+								'flags': [ 'primary' ]
+							} ),
+							new OO.ui.ButtonOptionWidget( 'c', {
+								'label': 'Constructive',
+								'flags': [ 'constructive' ]
+							} ),
+							new OO.ui.ButtonOptionWidget( 'd', {
+								'label': 'Destructive',
+								'flags': [ 'destructive' ]
+							} )
+						]
+					} ),
+					{ 'label': 'ButtonSelectWidget' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonSelectWidget( {
+						'disabled': true,
+						'items': [
+							new OO.ui.ButtonOptionWidget( 1, {
+								'label': 'Disabled',
+							} ),
+							new OO.ui.ButtonOptionWidget( 2, {
+								'label': 'state',
+							} ),
+							new OO.ui.ButtonOptionWidget( 3, {
+								'label': 'is',
+							} ),
+							new OO.ui.ButtonOptionWidget( 3, {
+								'label': 'inherited',
+							} )
+						]
+					} ),
+					{ 'label': 'ButtonSelectWidget (disabled)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonSelectWidget( {
+						'items': [
+							new OO.ui.ButtonOptionWidget( 1, {
+								'label': 'Disabled',
+								'disabled': true
+							} ),
+							new OO.ui.ButtonOptionWidget( 2, {
+								'label': 'Enabled'
+							} ),
+							new OO.ui.ButtonOptionWidget( 3, {
+								'label': 'Disabled',
+								'disabled': true
+							} ),
+							new OO.ui.ButtonOptionWidget( 3, {
+								'label': 'Enabled'
+							} )
+						]
+					} ),
+					{ 'label': 'ButtonSelectWidget (disabled items)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ToggleButtonWidget( { 'label': 'Toggle' } ),
+					{ 'label': 'ToggleButtonWidget' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ToggleButtonWidget( { 'icon': 'next' } ),
+					{ 'label': 'ToggleButtonWidget (icon only)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ToggleSwitchWidget(),
+					{ 'label': 'ToggleSwitchWidget' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ToggleSwitchWidget( { 'disabled': true } ),
+					{ 'label': 'ToggleSwitchWidget (disabled)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.CheckboxInputWidget( {
+						'value': true
+					} ),
+					{
+						'align': 'inline',
+						'label': 'CheckboxInputWidget'
+					}
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.CheckboxInputWidget( {
+						'value': true,
 						'disabled': true
 					} ),
-					'items': [
-						new OO.ui.ButtonOptionWidget( 1, {
-							'label': 'Disabled',
-						} ),
-						new OO.ui.ButtonOptionWidget( 2, {
-							'label': 'state',
-						} ),
-						new OO.ui.ButtonOptionWidget( 3, {
-							'label': 'is',
-						} ),
-						new OO.ui.ButtonOptionWidget( 3, {
-							'label': 'inherited',
-						} )
-					]
-				},
-				{
-					'widget': new OO.ui.ButtonSelectWidget(),
-					'items': [
-						new OO.ui.ButtonOptionWidget( 1, {
-							'label': 'Disabled',
-							'disabled': true
-						} ),
-						new OO.ui.ButtonOptionWidget( 2, {
-							'label': 'Enabled'
-						} ),
-						new OO.ui.ButtonOptionWidget( 3, {
-							'label': 'Disabled',
-							'disabled': true
-						} ),
-						new OO.ui.ButtonOptionWidget( 3, {
-							'label': 'Enabled'
-						} )
-					]
-				},
-				{
-					'label': 'ToggleButtonWidget',
-					'widget': new OO.ui.ToggleButtonWidget( { 'label': 'Toggle' } )
-				},
-				{
-					'widget': new OO.ui.ToggleButtonWidget( {
-						'icon': 'next'
-					} )
-				},
-				{
-					'label': 'ToggleSwitchWidget',
-					'widget': new OO.ui.ToggleSwitchWidget()
-				},
-				{
-					'widget': new OO.ui.ToggleSwitchWidget( {
-						'offLabel': 'Disabled',
-						'disabled': true
-					} )
-				},
-				{
-					'label': 'CheckboxInputWidget',
-					'widget': new OO.ui.CheckboxInputWidget( {
-						'value': true
-					} )
-				},
-				{
-					'label': 'CheckboxWidget',
-					'widget': new OO.ui.CheckboxWidget( {
-						'value': true,
-						'label': 'Labeled'
-					} )
-				},
-				{
-					'widget': new OO.ui.CheckboxWidget( {
-						'value': true,
-						'label': 'Disabled',
-						'disabled': true
-					} )
-				},
-				{
-					'label': 'TextInputWidget',
-					'widget': new OO.ui.TextInputWidget( { 'value': 'Text input' } )
-				},
-				{
-					'widget': new OO.ui.TextInputWidget( {
-						'placeholder': 'Placeholder'
-					} )
-				},
-				{
-					'widget': new OO.ui.TextInputWidget( {
+					{
+						'align': 'inline',
+						'label': 'CheckboxInputWidget (disabled)'
+					}
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.TextInputWidget( { 'value': 'Text input' } ),
+					{ 'label': 'TextInputWidget' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.TextInputWidget( { 'placeholder': 'Placeholder' } ),
+					{ 'label': 'TextInputWidget (placeholder)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.TextInputWidget( {
 						'value': 'Readonly',
 						'readOnly': true
-					} )
-				},
-				{
-					'widget': new OO.ui.TextInputWidget( {
+					} ),
+					{ 'label': 'TextInputWidget (readonly)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.TextInputWidget( {
 						'value': 'Disabled',
 						'disabled': true
-					} )
-				},
-				{
-					'widget': new OO.ui.TextInputWidget( {
+					} ),
+					{ 'label': 'TextInputWidget (disabled)' }
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.TextInputWidget( {
 						'multiline': true,
 						'value': 'Multiline'
-					} )
-				}
+					} ),
+					{ 'label': 'TextInputWidget (multiline)' }
+				)
 			]
 		};
 
 	for ( name in sections ) {
 		fieldset = new OO.ui.FieldsetLayout( { 'label': name } );
-		items = sections[name];
-		for ( i = 0, len = items.length; i < len; i++ ) {
-			item = items[i];
-			if ( item.items ) {
-				item.widget.addItems( item.items );
-			}
-			fieldset.$element.append(
-				$( '<div>' )
-					.addClass( 'oo-ui-demo-item' )
-					.append(
-						$( '<div>' )
-							.addClass( 'oo-ui-demo-item-label' )
-							.text( item.label ),
-						$( '<div>' )
-							.addClass( 'oo-ui-demo-item-widget' )
-							.append( item.widget.$element ),
-						OO.ui.demo.buildConsole( item )
-					)
-			);
-		}
+		fieldset.addItems( sections[name] );
+		// TODO: Update buildConsole to add consoles to fieldsets full of field layouts
+		// OO.ui.demo.buildConsole( item );
 		$demo.append( fieldset.$element );
 	}
 } );

src/OO.ui.Element.js
@@ -20,7 +20,7 @@ OO.ui.Element = function OoUiElement( config ) {
 	this.elementGroup = null;
 
 	// Initialization
-	if ( Array.isArray( config.classes ) ) {
+	if ( $.isArray( config.classes ) ) {
 		this.$element.addClass( config.classes.join( ' ' ) );
 	}
 	if ( config.$content ) {

src/OO.ui.ToolFactory.js
@@ -71,7 +71,7 @@ OO.ui.ToolFactory.prototype.extract = function ( collection, used ) {
 				}
 			}
 		}
-	} else if ( Array.isArray( collection ) ) {
+	} else if ( $.isArray( collection ) ) {
 		for ( i = 0, len = collection.length; i < len; i++ ) {
 			item = collection[i];
 			// Allow plain strings as shorthand for named tools

src/OO.ui.ToolGroup.js
@@ -27,7 +27,7 @@ OO.ui.ToolGroup = function OoUiToolGroup( toolbar, config ) {
 	OO.ui.Widget.call( this, config );
 
 	// Mixin constructors
-	OO.ui.GroupElement.call( this, this.$( '<div>' ) );
+	OO.ui.GroupElement.call( this, this.$( '<div>' ), config );
 
 	// Properties
 	this.toolbar = toolbar;

src/OO.ui.Toolbar.js
@@ -8,20 +8,20 @@
  *
  * @constructor
  * @param {OO.Factory} toolFactory Factory for creating tools
- * @param {Object} [options] Configuration options
+ * @param {Object} [config] Configuration options
  * @cfg {boolean} [actions] Add an actions section opposite to the tools
  * @cfg {boolean} [shadow] Add a shadow below the toolbar
  */
-OO.ui.Toolbar = function OoUiToolbar( toolFactory, options ) {
+OO.ui.Toolbar = function OoUiToolbar( toolFactory, config ) {
 	// Configuration initialization
-	options = options || {};
+	config = config || {};
 
 	// Parent constructor
-	OO.ui.Element.call( this, options );
+	OO.ui.Element.call( this, config );
 
 	// Mixin constructors
 	OO.EventEmitter.call( this );
-	OO.ui.GroupElement.call( this, this.$( '<div>' ) );
+	OO.ui.GroupElement.call( this, this.$( '<div>' ), config );
 
 	// Properties
 	this.toolFactory = toolFactory;
@@ -39,12 +39,12 @@ OO.ui.Toolbar = function OoUiToolbar( toolFactory, options ) {
 	// Initialization
 	this.$group.addClass( 'oo-ui-toolbar-tools' );
 	this.$bar.addClass( 'oo-ui-toolbar-bar' ).append( this.$group );
-	if ( options.actions ) {
+	if ( config.actions ) {
 		this.$actions.addClass( 'oo-ui-toolbar-actions' );
 		this.$bar.append( this.$actions );
 	}
 	this.$bar.append( '<div style="clear:both"></div>' );
-	if ( options.shadow ) {
+	if ( config.shadow ) {
 		this.$bar.append( '<div class="oo-ui-toolbar-shadow"></div>' );
 	}
 	this.$element.addClass( 'oo-ui-toolbar' ).append( this.$bar );

src/elements/OO.ui.ClippableElement.js
@@ -6,8 +6,12 @@
  *
  * @constructor
  * @param {jQuery} $clippable Nodes to clip, assigned to #$clippable
+ * @param {Object} [config] Configuration options
  */
-OO.ui.ClippableElement = function OoUiClippableElement( $clippable ) {
+OO.ui.ClippableElement = function OoUiClippableElement( $clippable, config ) {
+	// Configuration initialization
+	config = config || {};
+
 	// Properties
 	this.$clippable = $clippable;
 	this.clipping = false;

src/elements/OO.ui.FlaggableElement.js
@@ -54,7 +54,7 @@ OO.ui.FlaggableElement.prototype.setFlags = function ( flags ) {
 	var i, len, flag,
 		classPrefix = 'oo-ui-flaggableElement-';
 
-	if ( Array.isArray( flags ) ) {
+	if ( $.isArray( flags ) ) {
 		for ( i = 0, len = flags.length; i < len; i++ ) {
 			flag = flags[i];
 			// Set

src/elements/OO.ui.GroupElement.js
@@ -49,7 +49,7 @@ OO.ui.GroupElement.prototype.addItems = function ( items, index ) {
 		item = items[i];
 
 		// Check if item exists then remove it first, effectively "moving" it
-		currentIndex = this.items.indexOf( item );
+		currentIndex = $.inArray( item, this.items );
 		if ( currentIndex >= 0 ) {
 			this.removeItems( [ item ] );
 			// Adjust index to compensate for removal
@@ -100,7 +100,7 @@ OO.ui.GroupElement.prototype.removeItems = function ( items ) {
 	// Remove specific items
 	for ( i = 0, len = items.length; i < len; i++ ) {
 		item = items[i];
-		index = this.items.indexOf( item );
+		index = $.inArray( item, this.items );
 		if ( index !== -1 ) {
 			if ( this.aggregate ) {
 				item.disconnect( this );

commit 6f65873ab8d5a85636795d8e11c783b355cc82fa

demo/widgets: Bring dev console back
Follows-up 424b40373e4 which broke it.

Change-Id: I405f76456e13ae48744c68123ae64245daa36eb3

demos/demo.js
@@ -1,15 +1,14 @@
 ( function ( $ ) {
 
 	/**
-	 * @param {Object} item
-	 * @param {string} key Name of key in item
+	 * @param {OO.ui.Element} item
+	 * @param {string} key Variable name for item
 	 * @param {string} [item.label=""]
 	 * @return {jQuery} Console interface element
 	 */
 	function buildConsole( item, key ) {
 		var $toggle, $log, $label, $input, $submit, $console, $form,
-			console = window.console,
-			name = item.label || item[ key ].constructor.name;
+			console = window.console;
 
 		function exec( str ) {
 			var func, ret;
@@ -19,7 +18,7 @@
 			}
 			try {
 				func = new Function( key, 'item', str );
-				ret = { value: func( item[ key ], item ) };
+				ret = { value: func( item, item ) };
 			} catch ( error ) {
 				ret = {
 					value: undefined,
@@ -36,13 +35,9 @@
 			$input.val( '' ).focus();
 			result = exec( val );
 
-			if ( result.value && $.type( result.value ) === 'object' ) {
-				logval = '[object ' + ( result.value.constructor.name || 'Object' ) + ']';
-			} else {
-				logval = String( result.value );
-				if ( logval === '' ) {
-					logval = '""';
-				}
+			logval = String( result.value );
+			if ( logval === '' ) {
+				logval = '""';
 			}
 
 			$log.append(
@@ -59,12 +54,12 @@
 			}
 
 			if ( console && console.log ) {
-				console.log( '[Console "' + name + '"]', result.value );
+				console.log( '[demo]', result.value );
 				if ( result.error ) {
 					if ( console.error ) {
-						console.error( '[Console "' + name + '"]', String( result.error ), result.error );
+						console.error( '[demo]', String( result.error ), result.error );
 					} else {
-						console.log( '[Console "' + name + '"] Error: ', result.error );
+						console.log( '[demo] Error: ', result.error );
 					}
 				}
 			}
@@ -82,9 +77,9 @@
 				if ( $input.is( ':visible' ) ) {
 					$input.focus();
 					if ( console && console.log ) {
-						window[ '$' + key ] = item[ key ];
-						console.log( '[Console "' + name + '"]', 'Global $' + key + ' has been set' );
-						console.log( '[Console "' + name + '"]', item[ key ] );
+						window[ '$' + key ] = item;
+						console.log( '[demo]', 'Global $' + key + ' has been set' );
+						console.log( '[demo]', item );
 					}
 				}
 			} );
@@ -97,7 +92,7 @@
 
 		$input = $( '<input>' )
 			.addClass( 'oo-ui-demo-console-input' )
-			.prop( 'placeholder', '... (predefined: ' + key + ', Object item)' );
+			.prop( 'placeholder', '... (predefined: ' + key + ')' );
 
 		$submit = $( '<div>' )
 			.addClass( 'oo-ui-demo-console-submit' )

demos/widgets.js
@@ -1,5 +1,5 @@
 $( function () {
-	var name, fieldset,
+	var name, fieldset, fieldlayouts,
 		$demo = $( '.oo-ui-demo' ),
 		sections = {
 			'Widgets': [
@@ -347,11 +347,18 @@ $( function () {
 			]
 		};
 
+	function insertConsole( fieldLayout ) {
+		fieldLayout.$element.append(
+			OO.ui.demo.buildConsole( fieldLayout.field, 'widget' )
+		);
+	}
+
 	for ( name in sections ) {
+		fieldlayouts = sections[ name ];
 		fieldset = new OO.ui.FieldsetLayout( { 'label': name } );
-		fieldset.addItems( sections[name] );
-		// TODO: Update buildConsole to add consoles to fieldsets full of field layouts
-		// OO.ui.demo.buildConsole( item, 'widget' );
+		$.map( fieldlayouts, insertConsole );
+		fieldset.addItems( fieldlayouts );
+
 		$demo.append( fieldset.$element );
 	}
 } );