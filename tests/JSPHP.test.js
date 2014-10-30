f2e316a71f0fe7e09addecbf44c5064a5335b350
Bartosz Dziewoński <matma.rex@gmail.com>
2014-10-30 21:54:04 +0100

    JS/PHP comparison test suite tweaks
    Follow-up to I52bd98ee.
    
    * Change CRLF to LF line endings.
    * Get rid of loopfunc in JSPHP.test.js.
    
    Change-Id: Ib4166fac75cef32250b3c7756a47036472cc511c

--- tests/JSPHP.test.js
+++ tests/JSPHP.test.js
@@ -1,36 +1,36 @@
 QUnit.module( 'JSPHP' );
 
 ( function () {
-	var suite, klassName;
+	// Generate some tests based on the test suite data and HTML from PHP version.
+
 	function expandClass( klass ) {
 		return OO.ui[klass];
 	}
 
-	// Generate some tests based on the test suite data and HTML from PHP version.
-	suite = OO.ui.JSPHPTestSuite;
-	for ( klassName in suite ) {
-		/*jshint loopfunc:true */
-		( function ( klassName ) {
-			QUnit.test( klassName, suite[klassName].length, function ( assert ) {
-				var test, klass, instance, id, fromPhp, i, testName;
-				for ( i = 0; i < suite[klassName].length; i++ ) {
-					test = suite[klassName][i];
-					klass = expandClass( test.class );
-					// jscs:disable requireCapitalizedConstructors
-					instance = new klass( test.config );
+	function makeTest( klassName, suite ) {
+		QUnit.test( klassName, suite[klassName].length, function ( assert ) {
+			var test, klass, instance, id, fromPhp, i, testName;
+			for ( i = 0; i < suite[klassName].length; i++ ) {
+				test = suite[klassName][i];
+				klass = expandClass( test.class );
+				// jscs:disable requireCapitalizedConstructors
+				instance = new klass( test.config );
 
-					id = 'JSPHPTestSuite_' + klassName + i;
-					fromPhp = document.getElementById( id );
-					fromPhp.removeAttribute( 'id' );
-					instance.$element.insertBefore( fromPhp );
+				id = 'JSPHPTestSuite_' + klassName + i;
+				fromPhp = document.getElementById( id );
+				fromPhp.removeAttribute( 'id' );
+				instance.$element.insertBefore( fromPhp );
 
-					// Updating theme classes is normally debounced, we need to do it immediately
-					instance.debouncedUpdateThemeClasses();
+				// Updating theme classes is normally debounced, we need to do it immediately
+				instance.debouncedUpdateThemeClasses();
 
-					testName = JSON.stringify( test.config );
-					assert.equalDomElement( instance.$element[0], fromPhp, testName );
-				}
-			} );
-		} )( klassName );
+				testName = JSON.stringify( test.config );
+				assert.equalDomElement( instance.$element[0], fromPhp, testName );
+			}
+		} );
+	}
+
+	for ( var klassName in OO.ui.JSPHPTestSuite ) {
+		makeTest( klassName, OO.ui.JSPHPTestSuite );
 	}
 } )();
