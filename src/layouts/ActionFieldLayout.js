2b76d688c569d27da3c7a266e0019a62c6d6d19c
Moriel Schottlender <moriel@gmail.com>
2014-11-24 17:14:22 -0500

    Add an ActionFieldLayout
    Add an extension to FieldLayout that allows for an inline button
    after the field. Demos added.
    
    Bug: T76634
    Change-Id: I2cdf9a43bc8fe9d681c12df6b427e25934a65200

---
+++ src/layouts/ActionFieldLayout.js
@@ -0,0 +1,44 @@
+/**
+ * Layout made of a field, a button, and an optional label.
+ *
+ * @class
+ * @extends OO.ui.FieldLayout
+ *
+ * @constructor
+ * @param {OO.ui.Widget} fieldWidget Field widget
+ * @param {OO.ui.ButtonWidget} buttonWidget Button widget
+ * @param {Object} [config] Configuration options
+ * @cfg {string} [align='left'] Alignment mode, either 'left', 'right', 'top' or 'inline'
+ * @cfg {string} [help] Explanatory text shown as a '?' icon.
+ */
+OO.ui.ActionFieldLayout = function OoUiActionFieldLayout( fieldWidget, buttonWidget, config ) {
+	// Configuration initialization
+	config = $.extend( { align: 'left' }, config );
+
+	// Properties (must be set before parent constructor, which calls #getTagName)
+	this.fieldWidget = fieldWidget;
+	this.buttonWidget = buttonWidget;
+
+	// Parent constructor
+	OO.ui.ActionFieldLayout.super.call( this, fieldWidget, config );
+
+	// Mixin constructors
+	OO.ui.LabelElement.call( this, config );
+
+	// Properties
+	this.$button = this.$( '<div>' )
+		.addClass( 'oo-ui-actionFieldLayout-button' )
+		.append( this.buttonWidget.$element );
+
+	this.$input = this.$( '<div>' )
+		.addClass( 'oo-ui-actionFieldLayout-input' )
+		.append( this.fieldWidget.$element );
+
+	this.$field
+		.addClass( 'oo-ui-actionFieldLayout' )
+		.append( this.$input, this.$button );
+};
+
+/* Setup */
+
+OO.inheritClass( OO.ui.ActionFieldLayout, OO.ui.FieldLayout );
