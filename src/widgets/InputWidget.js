df4e5b4b024ad9c909cf8b53b97f015251915fd5
Bartosz Dziewoński <matma.rex@gmail.com>
2014-11-29 19:50:06 +0100

    InputWidget: Update DOM value before firing 'change' event
    It seems sensible not to fire events while the object is in
    inconsistent state.
    
    It was previously done in this order before 8b32c7d2 was done to fix
    the behavior when #cleanUpValue is not a no-op.
    
    Change-Id: I5cc59b5c2682ece6b0df303c07b0c4dfa32d95ed

--- src/widgets/InputWidget.js
+++ src/widgets/InputWidget.js
@@ -111,14 +111,14 @@ OO.ui.InputWidget.prototype.setRTL = function ( isRTL ) {
  */
 OO.ui.InputWidget.prototype.setValue = function ( value ) {
 	value = this.cleanUpValue( value );
+	// Update the DOM if it has changed. Note that with cleanUpValue, it
+	// is possible for the DOM value to change without this.value changing.
+	if ( this.$input.val() !== value ) {
+		this.$input.val( value );
+	}
 	if ( this.value !== value ) {
 		this.value = value;
 		this.emit( 'change', this.value );
-	}
-	// Update the DOM if it has changed. Note that with cleanUpValue, it
-	// is possible for the DOM value to change without this.value changing.
-	if ( this.$input.val() !== this.value ) {
-		this.$input.val( this.value );
 	}
 	return this;
 };
