65d9f8e07af339f4f0ddf47f9febeee69142fc6c
Kirsten Menger-Anderson <kmenger@wikimedia.org>
2015-02-25 15:38:44 -0800

    MenuOptionWidget: Add description
    Change-Id: Ib7832ab103ba5e2b3adb2c92e9d6be9a04cc3c8f

--- src/widgets/MenuOptionWidget.js
+++ src/widgets/MenuOptionWidget.js
@@ -1,5 +1,9 @@
 /**
- * Item of an OO.ui.MenuSelectWidget.
+ * MenuOptionWidget is an option widget that looks like a menu item. The class is used with
+ * OO.ui.MenuSelectWidget to create a menu of mutually exclusive options. Please see
+ * the [OOjs UI documentation on MediaWiki] [1] for more information.
+ *
+ * [1]: https://www.mediawiki.org/wiki/OOjs_UI/Widgets/Selects_and_Options#Menu_selects_and_options
  *
  * @class
  * @extends OO.ui.DecoratedOptionWidget
