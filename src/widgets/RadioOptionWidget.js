023cddce92cbd696abf6938f7caa63c5829e3874
Kirsten Menger-Anderson <kmenger@wikimedia.org>
2015-02-25 14:48:41 -0800

    RadioOptionWidget: Add description
    Change-Id: I84e99e459d97def23fb2571b6939b0310a235067

--- src/widgets/RadioOptionWidget.js
+++ src/widgets/RadioOptionWidget.js
@@ -1,7 +1,9 @@
 /**
- * Option widget that looks like a radio button.
+ * RadioOptionWidget is an option widget that looks like a radio button.
+ * The class is used with OO.ui.RadioSelectWidget to create a selection of radio options.
+ * Please see the [OOjs UI documentation on MediaWiki] [1] for more information.
  *
- * Use together with OO.ui.RadioSelectWidget.
+ * [1]: https://www.mediawiki.org/wiki/OOjs_UI/Widgets/Selects_and_Options#Button_selects_and_option
  *
  * @class
  * @extends OO.ui.OptionWidget
