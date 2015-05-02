037658862ffc21c21494d16138f365a6f9ef9000
Roan Kattouw <roan.kattouw@gmail.com>
2015-05-01 18:31:51 -0700

    core: Add OO.ui.debounce() utility
    Moved from ve.debounce(); originally ported from underscore.js
    
    Change-Id: Iefcba48819cc71436e785cf98c7b74d8c7d1fbaf

--- src/core.js
+++ src/core.js
@@ -137,6 +137,38 @@ OO.ui.contains = function ( containers, contained, matchContainers ) {
 };
 
 /**
+ * Return a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing.
+ *
+ * Ported from: http://underscorejs.org/underscore.js
+ *
+ * @param {Function} func
+ * @param {number} wait
+ * @param {boolean} immediate
+ * @return {Function}
+ */
+OO.ui.debounce = function ( func, wait, immediate ) {
+	var timeout;
+	return function () {
+		var context = this,
+			args = arguments,
+			later = function () {
+				timeout = null;
+				if ( !immediate ) {
+					func.apply( context, args );
+				}
+			};
+		if ( immediate && !timeout ) {
+			func.apply( context, args );
+		}
+		clearTimeout( timeout );
+		timeout = setTimeout( later, wait );
+	};
+};
+
+/**
  * Reconstitute a JavaScript object corresponding to a widget created by
  * the PHP implementation.
  *
