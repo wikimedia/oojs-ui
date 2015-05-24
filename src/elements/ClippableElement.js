wikimedia/oojs-ui

commit f5823566ca03019a1811c729c0e7839cddcfc2de

ClippableElement: Handle clipping with left edge
Bug: T76474
Change-Id: I0c2d9800a1c0bdbe523c978307546ec205202681

src/elements/ClippableElement.js
@@ -169,10 +169,16 @@ OO.ui.ClippableElement.prototype.clip = function () {
 		ccOffset = $container.offset() || { top: 0, left: 0 },
 		ccHeight = $container.innerHeight() - buffer,
 		ccWidth = $container.innerWidth() - buffer,
+		cHeight = this.$clippable.outerHeight() + buffer,
+		cWidth = this.$clippable.outerWidth() + buffer,
 		scrollTop = this.$clippableScroller.scrollTop(),
 		scrollLeft = this.$clippableScroller.scrollLeft(),
-		desiredWidth = ( ccOffset.left + scrollLeft + ccWidth ) - cOffset.left,
-		desiredHeight = ( ccOffset.top + scrollTop + ccHeight ) - cOffset.top,
+		desiredWidth = cOffset.left < 0 ?
+			cWidth + cOffset.left :
+			( ccOffset.left + scrollLeft + ccWidth ) - cOffset.left,
+		desiredHeight = cOffset.top < 0 ?
+			cHeight + cOffset.top :
+			( ccOffset.top + scrollTop + ccHeight ) - cOffset.top,
 		naturalWidth = this.$clippable.prop( 'scrollWidth' ),
 		naturalHeight = this.$clippable.prop( 'scrollHeight' ),
 		clipWidth = desiredWidth < naturalWidth,

commit dc02cf67aceca939dae427c6f7335f7372d0b871

ClippableElement: Fix behavior of long clippables
Revert misguided bit of code from f5823566 which caused funny
behavior when the clippable was higher than the screen.

I am sincerely hoping that we will never need clipping with the top
edge of the screen (or: popups that pop up upwards), because it will
not work. (Conversely, clippables wider than the screen do not work
correctly now, but they can be clipped with either right or left
edge.)

If we ever decide we need that, then ClippableElement will need to
know to which edge the clippable is anchored to be able to
reasonably decide how to clip it.

Bug: T98506
Change-Id: I3b5925999e0ee5f46f3865abf17f184a5b7d0a55

src/elements/ClippableElement.js
@@ -167,16 +167,13 @@ OO.ui.ClippableElement.prototype.clip = function () {
 		ccOffset = $container.offset() || { top: 0, left: 0 },
 		ccHeight = $container.innerHeight() - buffer,
 		ccWidth = $container.innerWidth() - buffer,
-		cHeight = this.$clippable.outerHeight() + buffer,
 		cWidth = this.$clippable.outerWidth() + buffer,
 		scrollTop = this.$clippableScroller.scrollTop(),
 		scrollLeft = this.$clippableScroller.scrollLeft(),
 		desiredWidth = cOffset.left < 0 ?
 			cWidth + cOffset.left :
 			( ccOffset.left + scrollLeft + ccWidth ) - cOffset.left,
-		desiredHeight = cOffset.top < 0 ?
-			cHeight + cOffset.top :
-			( ccOffset.top + scrollTop + ccHeight ) - cOffset.top,
+		desiredHeight = ( ccOffset.top + scrollTop + ccHeight ) - cOffset.top,
 		naturalWidth = this.$clippable.prop( 'scrollWidth' ),
 		naturalHeight = this.$clippable.prop( 'scrollHeight' ),
 		clipWidth = desiredWidth < naturalWidth,