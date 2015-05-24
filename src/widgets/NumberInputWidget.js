a7dcc1c19081b4062a9be2dbfbcfacdea2206f0a
Brad Jorsch <bjorsch@wikimedia.org>
2015-05-24 15:23:09 +0200

    Add NumberInputWidget for numeric input
    Bug: T92263
    Change-Id: I64f04e091bacfdf8b391daf663864d156efdc72d

---
+++ src/widgets/NumberInputWidget.js
@@ -0,0 +1,356 @@
+/**
+ * NumberInputWidgets combine a {@link OO.ui.TextInputWidget text input} (where a value
+ * can be entered manually) and two {@link OO.ui.ButtonWidget button widgets}
+ * (to adjust the value in increments) to allow the user to enter a number.
+ *
+ *     @example
+ *     // Example: A NumberInputWidget.
+ *     var numberInput = new OO.ui.NumberInputWidget( {
+ *         label: 'NumberInputWidget',
+ *         input: { value: 5, min: 1, max: 10 }
+ *     } );
+ *     $( 'body' ).append( numberInput.$element );
+ *
+ * @class
+ * @extends OO.ui.Widget
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [input] Configuration options to pass to the {@link OO.ui.TextInputWidget text input widget}.
+ * @cfg {Object} [minusButton] Configuration options to pass to the {@link OO.ui.ButtonWidget decrementing button widget}.
+ * @cfg {Object} [plusButton] Configuration options to pass to the {@link OO.ui.ButtonWidget incrementing button widget}.
+ * @cfg {boolean} [isInteger=false] Whether the field accepts only integer values.
+ * @cfg {number} [min=-Infinity] Minimum allowed value
+ * @cfg {number} [max=Infinity] Maximum allowed value
+ * @cfg {number} [step=1] Delta when using the buttons or up/down arrow keys
+ * @cfg {number|null} [pageStep] Delta when using the page-up/page-down keys. Defaults to 10 times #step.
+ */
+OO.ui.NumberInputWidget = function OoUiNumberInputWidget( config ) {
+	// Configuration initialization
+	config = $.extend( {
+		isInteger: false,
+		min: -Infinity,
+		max: Infinity,
+		step: 1,
+		pageStep: null
+	}, config );
+
+	// Parent constructor
+	OO.ui.NumberInputWidget.super.call( this, config );
+
+	// Properties
+	this.input = new OO.ui.TextInputWidget( $.extend(
+		{
+			disabled: this.isDisabled()
+		},
+		config.input
+	) );
+	this.minusButton = new OO.ui.ButtonWidget( $.extend(
+		{
+			disabled: this.isDisabled(),
+			tabIndex: -1
+		},
+		config.minusButton,
+		{
+			classes: [ 'oo-ui-numberInputWidget-minusButton' ],
+			label: '−'
+		}
+	) );
+	this.plusButton = new OO.ui.ButtonWidget( $.extend(
+		{
+			disabled: this.isDisabled(),
+			tabIndex: -1
+		},
+		config.plusButton,
+		{
+			classes: [ 'oo-ui-numberInputWidget-plusButton' ],
+			label: '+'
+		}
+	) );
+
+	// Events
+	this.input.connect( this, {
+		change: this.emit.bind( this, 'change' ),
+		enter: this.emit.bind( this, 'enter' )
+	} );
+	this.input.$input.on( {
+		keypress: this.onKeyPress.bind( this ),
+		'wheel mousewheel DOMMouseScroll': this.onWheel.bind( this )
+	} );
+	this.plusButton.connect( this, {
+		click: [ 'onButtonClick', +1 ]
+	} );
+	this.minusButton.connect( this, {
+		click: [ 'onButtonClick', -1 ]
+	} );
+
+	// Initialization
+	this.setIsInteger( !!config.isInteger );
+	this.setRange( config.min, config.max );
+	this.setStep( config.step, config.pageStep );
+
+	this.$field = $( '<div>' ).addClass( 'oo-ui-numberInputWidget-field' )
+		.append(
+			this.minusButton.$element,
+			this.input.$element,
+			this.plusButton.$element
+		);
+	this.$element.addClass( 'oo-ui-numberInputWidget' ).append( this.$field );
+	this.input.setValidation( this.validateNumber.bind( this ) );
+};
+
+/* Setup */
+
+OO.inheritClass( OO.ui.NumberInputWidget, OO.ui.Widget );
+
+/* Events */
+
+/**
+ * A `change` event is emitted when the value of the input changes.
+ *
+ * @event change
+ */
+
+/**
+ * An `enter` event is emitted when the user presses 'enter' inside the text box.
+ *
+ * @event enter
+ */
+
+/* Methods */
+
+/**
+ * Set whether only integers are allowed
+ * @param {boolean} flag
+ */
+OO.ui.NumberInputWidget.prototype.setIsInteger = function ( flag ) {
+	this.isInteger = !!flag;
+	this.input.setValidityFlag();
+};
+
+/**
+ * Get whether only integers are allowed
+ * @return {boolean} Flag value
+ */
+OO.ui.NumberInputWidget.prototype.getIsInteger = function () {
+	return this.isInteger;
+};
+
+/**
+ * Set the range of allowed values
+ * @param {number} min Minimum allowed value
+ * @param {number} max Maximum allowed value
+ */
+OO.ui.NumberInputWidget.prototype.setRange = function ( min, max ) {
+	if ( min > max ) {
+		throw new Error( 'Minimum (' + min + ') must not be greater than maximum (' + max + ')' );
+	}
+	this.min = min;
+	this.max = max;
+	this.input.setValidityFlag();
+};
+
+/**
+ * Get the current range
+ * @return {number[]} Minimum and maximum values
+ */
+OO.ui.NumberInputWidget.prototype.getRange = function () {
+	return [ this.min, this.max ];
+};
+
+/**
+ * Set the stepping deltas
+ * @param {number} step Normal step
+ * @param {number|null} pageStep Page step. If null, 10 * step will be used.
+ */
+OO.ui.NumberInputWidget.prototype.setStep = function ( step, pageStep ) {
+	if ( step <= 0 ) {
+		throw new Error( 'Step value must be positive' );
+	}
+	if ( pageStep === null ) {
+		pageStep = step * 10;
+	} else if ( pageStep <= 0 ) {
+		throw new Error( 'Page step value must be positive' );
+	}
+	this.step = step;
+	this.pageStep = pageStep;
+};
+
+/**
+ * Get the current stepping values
+ * @return {number[]} Step and page step
+ */
+OO.ui.NumberInputWidget.prototype.getStep = function () {
+	return [ this.step, this.pageStep ];
+};
+
+/**
+ * Get the current value of the widget
+ * @return {string}
+ */
+OO.ui.NumberInputWidget.prototype.getValue = function () {
+	return this.input.getValue();
+};
+
+/**
+ * Get the current value of the widget as a number
+ * @return {number} May be NaN, or an invalid number
+ */
+OO.ui.NumberInputWidget.prototype.getNumericValue = function () {
+	return +this.input.getValue();
+};
+
+/**
+ * Set the value of the widget
+ * @param {string} value Invalid values are allowed
+ */
+OO.ui.NumberInputWidget.prototype.setValue = function ( value ) {
+	this.input.setValue( value );
+};
+
+/**
+ * Adjust the value of the widget
+ * @param {number} delta Adjustment amount
+ */
+OO.ui.NumberInputWidget.prototype.adjustValue = function ( delta ) {
+	var n, v = this.getNumericValue();
+
+	delta = +delta;
+	if ( isNaN( delta ) || !isFinite( delta ) ) {
+		throw new Error( 'Delta must be a finite number' );
+	}
+
+	if ( isNaN( v ) ) {
+		n = 0;
+	} else {
+		n = v + delta;
+		n = Math.max( Math.min( n, this.max ), this.min );
+		if ( this.isInteger ) {
+			n = Math.round( n );
+		}
+	}
+
+	if ( n !== v ) {
+		this.setValue( n );
+	}
+};
+
+/**
+ * Validate input
+ * @private
+ * @param {string} value Field value
+ * @return {boolean}
+ */
+OO.ui.NumberInputWidget.prototype.validateNumber = function ( value ) {
+	var n = +value;
+	if ( isNaN( n ) || !isFinite( n ) ) {
+		return false;
+	}
+
+	/*jshint bitwise: false */
+	if ( this.isInteger && ( n | 0 ) !== n ) {
+		return false;
+	}
+	/*jshint bitwise: true */
+
+	if ( n < this.min || n > this.max ) {
+		return false;
+	}
+
+	return true;
+};
+
+/**
+ * Handle mouse click events.
+ *
+ * @private
+ * @param {number} dir +1 or -1
+ */
+OO.ui.NumberInputWidget.prototype.onButtonClick = function ( dir ) {
+	this.adjustValue( dir * this.step );
+};
+
+/**
+ * Handle mouse wheel events.
+ *
+ * @private
+ * @param {jQuery.Event} event
+ */
+OO.ui.NumberInputWidget.prototype.onWheel = function ( event ) {
+	var delta = 0;
+
+	// Standard 'wheel' event
+	if ( event.originalEvent.deltaMode !== undefined ) {
+		this.sawWheelEvent = true;
+	}
+	if ( event.originalEvent.deltaY ) {
+		delta = -event.originalEvent.deltaY;
+	} else if ( event.originalEvent.deltaX ) {
+		delta = event.originalEvent.deltaX;
+	}
+
+	// Non-standard events
+	if ( !this.sawWheelEvent ) {
+		if ( event.originalEvent.wheelDeltaX ) {
+			delta = -event.originalEvent.wheelDeltaX;
+		} else if ( event.originalEvent.wheelDeltaY ) {
+			delta = event.originalEvent.wheelDeltaY;
+		} else if ( event.originalEvent.wheelDelta ) {
+			delta = event.originalEvent.wheelDelta;
+		} else if ( event.originalEvent.detail ) {
+			delta = -event.originalEvent.detail;
+		}
+	}
+
+	if ( delta ) {
+		this.adjustValue( Math.sign( delta ) * this.step );
+	}
+
+	return false;
+};
+
+/**
+ * Handle key press events.
+ *
+ *
+ * @private
+ * @param {jQuery.Event} e Key press event
+ */
+OO.ui.NumberInputWidget.prototype.onKeyPress = function ( e ) {
+	if ( !this.isDisabled() ) {
+		switch ( e.keyCode ) {
+			case OO.ui.Keys.UP:
+				this.adjustValue( this.step );
+				return false;
+			case OO.ui.Keys.DOWN:
+				this.adjustValue( -this.step );
+				return false;
+			case OO.ui.Keys.PAGEUP:
+				this.adjustValue( this.pageStep );
+				return false;
+			case OO.ui.Keys.PAGEDOWN:
+				this.adjustValue( -this.pageStep );
+				return false;
+		}
+	}
+};
+
+/**
+ * @inheritdoc
+ */
+OO.ui.NumberInputWidget.prototype.setDisabled = function ( disabled ) {
+	// Parent method
+	OO.ui.NumberInputWidget.super.prototype.setDisabled.call( this, disabled );
+
+	if ( this.input ) {
+		this.input.setDisabled( this.isDisabled() );
+	}
+	if ( this.minusButton ) {
+		this.minusButton.setDisabled( this.isDisabled() );
+	}
+	if ( this.plusButton ) {
+		this.plusButton.setDisabled( this.isDisabled() );
+	}
+
+	return this;
+};
