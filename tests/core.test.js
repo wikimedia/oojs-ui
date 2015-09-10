9fdb52ebd983aa904e541b86a6073df638b4870d
Ed Sanders <esanders@wikimedia.org>
2015-09-10 20:44:23 +0100

    Rewrite isFocusableElement for performance and correctness
    Improve performance by doing cheap tests first and returning
    immediately, also avoid jQuery when possible.
    
    Improve correctness by returning true for any element with a
    tabIndex or contentEditable.
    
    Avoid browser bugs with tabIndex by using $.prop.
    
    Add area[href] as focusable.
    
    Bug: T69156
    Change-Id: Icdbbffa7112a56849118ffb73a85e37e20844500

--- tests/core.test.js
+++ tests/core.test.js
@@ -16,3 +16,77 @@ QUnit.test( 'isSafeUrl', 10, function ( assert ) {
 	assert.ok( OO.ui.isSafeUrl( '/relative' ) );
 	assert.ok( OO.ui.isSafeUrl( '/wiki/Extra:Colon' ) );
 } );
+
+QUnit.test( 'isFocusableElement', 10, function ( assert ) {
+	var i, $html, result,
+		cases = [
+			{
+				msg: 'Plain DIV',
+				html: '<div>foo</div>',
+				expected: false
+			},
+			{
+				msg: 'Tabindexed span',
+				html: '<span tabindex="3">foo</span>',
+				expected: true
+			},
+			{
+				msg: 'Hidden element',
+				html: '<div tabindex="3" style="display:none;">foo</div>',
+				expected: false
+			},
+			{
+				msg: 'Invalid tabindex',
+				html: '<div tabindex="wat">foo</div>',
+				expected: false
+			},
+			{
+				msg: 'Empty tabindex',
+				html: '<div tabindex="">foo</div>',
+				expected: false
+			},
+			{
+				msg: 'Text input',
+				html: '<input type="text">',
+				expected: true
+			},
+			{
+				msg: 'Disabled text input',
+				html: '<input type="text" disabled="disabled">',
+				expected: false
+			},
+			{
+				msg: 'Link',
+				html: '<a href="Foo">Bar</a>',
+				expected: true
+			},
+			{
+				msg: 'Link with empty href',
+				html: '<a href="">Bar</a>',
+				expected: true
+			},
+			{
+				msg: 'Link without href',
+				html: '<a>Bar</a>',
+				expected: false
+			},
+			{
+				msg: 'Link without href but with tabindex',
+				html: '<a tabindex="0">Bar</a>',
+				expected: true
+			},
+			{
+				msg: 'Link with negative tabindex',
+				html: '<a href="foo" tabindex="-1">Bar</a>',
+				expected: true
+			}
+		];
+
+	QUnit.expect( cases.length );
+	for ( i = 0; i < cases.length; i++ ) {
+		$html = $( cases[ i ].html ).appendTo( 'body' );
+		result = OO.ui.isFocusableElement( cases[ i ].selector ? $html.find( cases[ i ].selector ) : $html );
+		assert.strictEqual( result, cases[ i ].expected, cases[ i ].msg );
+		$html.remove();
+	}
+} );
