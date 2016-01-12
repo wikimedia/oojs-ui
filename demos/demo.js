30dfcbb4fa2723113835eb3161306378133065fb
Ed Sanders <esanders@wikimedia.org>
2016-01-12 17:51:36 +0000

    [DEPRECATING CHANGE] Move 'redirect' icon to 'articleRedirect' and cleanup
    Move the article icon alongside the other article icons in
    icons-content and rename and redraw for consistency.
    
    Change-Id: I697003e69687453532399e2571d9b9f7d8506600

--- demos/demo.js
+++ demos/demo.js
@@ -132,6 +132,7 @@ OO.ui.Demo.static.themes = {
 		fileSuffix: '-apex',
 		additionalSuffixes: [
 			'-icons-movement',
+			'-icons-content',
 			'-icons-interactions',
 			'-icons-moderation',
 			'-icons-editing-core',
