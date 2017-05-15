wikimedia/oojs-ui

commit 73521855861d4ef887af0b67533245c15e4dd942

FieldLayout: Remove the need for 'simulateLabelClick'
* Set 'for' attribute using 'getInputId' on $body
* Deprecate InputWidget#simulateLabelClick

Bug: T100219
Change-Id: I3424ecfd741b5e268513a5dd778e5082de8f7674

php/layouts/FieldLayout.php
@@ -110,6 +110,9 @@ class FieldLayout extends Layout {
 			array_merge( $config, [ 'titled' => $this->label ] ) );
 
 		// Initialization
+		if ( $hasInputWidget ) {
+			$this->body->setAttributes( [ 'for' => $this->fieldWidget->getInputId() ] );
+		}
 		$this
 			->addClasses( [ 'oo-ui-fieldLayout' ] )
 			->toggleClasses( [ 'oo-ui-fieldLayout-disable' ], $this->fieldWidget->isDisabled() )

src/layouts/FieldLayout.js
@@ -93,12 +93,12 @@ OO.ui.FieldLayout = function OoUiFieldLayout( fieldWidget, config ) {
 	}
 
 	// Events
-	if ( hasInputWidget ) {
-		this.$label.on( 'click', this.onLabelClick.bind( this ) );
-	}
 	this.fieldWidget.connect( this, { disable: 'onFieldDisable' } );
 
 	// Initialization
+	if ( hasInputWidget ) {
+		this.$body.attr( 'for', this.fieldWidget.getInputId() );
+	}
 	this.$element
 		.addClass( 'oo-ui-fieldLayout' )
 		.toggleClass( 'oo-ui-fieldLayout-disabled', this.fieldWidget.isDisabled() )
@@ -131,17 +131,6 @@ OO.mixinClass( OO.ui.FieldLayout, OO.ui.mixin.TitledElement );
  */
 OO.ui.FieldLayout.prototype.onFieldDisable = function ( value ) {
 	this.$element.toggleClass( 'oo-ui-fieldLayout-disabled', value );
-};
-
-/**
- * Handle label mouse click events.
- *
- * @private
- * @param {jQuery.Event} e Mouse click event
- */
-OO.ui.FieldLayout.prototype.onLabelClick = function () {
-	this.fieldWidget.simulateLabelClick();
-	return false;
 };
 
 /**

src/widgets/InputWidget.js
@@ -232,6 +232,7 @@ OO.ui.InputWidget.prototype.cleanUpValue = function ( value ) {
  * called directly.
  */
 OO.ui.InputWidget.prototype.simulateLabelClick = function () {
+	OO.ui.warnDeprecation( 'InputWidget: simulateLabelClick() is deprecated.' );
 	if ( !this.isDisabled() ) {
 		if ( this.$input.is( ':checkbox, :radio' ) ) {
 			this.$input.click();

commit edd824f54d0b019a670f4b56acce82b0ca750e62

Generalize .getInputId() for all widgets
No reason to limit this to InputWidget, the <label for> is nice for
accessibility so let's use it where we can. This method is now a part of
TabIndexedElement. Widget includes a fallback version of it, so that we
can just call it for anything.

The 'supportsSimpleLabel' property (previously meant to allow for the same
thing but broken at least since 73521855861d4ef887af0b67533245c15e4dd942,
which added a requirement for widgets to also implement .getInputId()),
is now removed.

The HTML specification places some arbitrary restrictions on which
elements a <label for> can be used with (so-called 'labelable elements'),
which TabIndexedElement follows.

Also resolve minor inconsistencies in the handling of this in LabelWidget
versus FieldLayout (basically, FieldLayout was able to handle some
non-InputWidgets like CapsuleMultiselectWidget, LabelWidget wasn't).

This should result in no behavior changes for code which previously worked.
For some widgets (e.g. TagMultiselectWidget), a FieldLayout will now use
a `<label for>` rather than manual click handling, but for a user the
effect is the same.

Change-Id: I4286ca7633e29fcef551cffd74d46aed52819f95

php/Widget.php
@@ -9,16 +9,6 @@ namespace OOUI;
  */
 class Widget extends Element {
 
-	/* Static Properties */
-
-	/**
-	 * Whether this widget will behave reasonably when wrapped in a HTML `<label>`. If this is true,
-	 * wrappers such as FieldLayout may use a `<label>`.
-	 *
-	 * @var boolean
-	 */
-	public static $supportsSimpleLabel = false;
-
 	/* Properties */
 
 	/**
@@ -70,6 +60,16 @@ class Widget extends Element {
 		$this->setAttributes( [ 'aria-disabled' => $this->disabled ? 'true' : 'false' ] );
 
 		return $this;
+	}
+
+	/**
+	 * Get an ID of a labelable node which is part of this widget, if any, to be used for
+	 * `<label for>` value.
+	 *
+	 * @return {string|null} The ID of the labelable node
+	 */
+	public function getInputId() {
+		return null;
 	}
 
 	public function getConfig( &$config ) {

php/layouts/FieldLayout.php
@@ -110,7 +110,7 @@ class FieldLayout extends Layout {
 			array_merge( $config, [ 'titled' => $this->label ] ) );
 
 		// Initialization
-		if ( $fieldWidget::$supportsSimpleLabel ) {
+		if ( $this->fieldWidget->getInputId() ) {
 			$this->label->setAttributes( [ 'for' => $this->fieldWidget->getInputId() ] );
 		}
 		$this

php/mixins/TabIndexedElement.php
@@ -86,4 +86,48 @@ trait TabIndexedElement {
 	public function getTabIndex() {
 		return $this->tabIndex;
 	}
+
+	/**
+	 * Get an ID of a focusable element of this widget, if any, to be used for `<label for>` value.
+	 *
+	 * If the element already has an ID then that is returned, otherwise unique ID is
+	 * generated, set on the element, and returned.
+	 *
+	 * @return {string|null} The ID of the focusable element
+	 */
+	public function getInputId() {
+		$id = $this->tabIndexed->getAttribute( 'id' );
+
+		if ( !$this->isLabelableNode( $this->tabIndexed ) ) {
+			return null;
+		}
+
+		if ( $id === null ) {
+			$id = Tag::generateElementId();
+			$this->tabIndexed->setAttributes( [ 'id' => $id ] );
+		}
+
+		return $id;
+	}
+
+	/**
+	 * Whether the node is 'labelable' according to the HTML spec
+	 * (i.e., whether it can be interacted with through a `<label for="…">`).
+	 * See: <https://html.spec.whatwg.org/multipage/forms.html#category-label>.
+	 *
+	 * @param Tag $tag
+	 * @return boolean
+	 */
+	private function isLabelableNode( Tag $tag ) {
+		$labelableTags = [ 'button', 'meter', 'output', 'progress', 'select', 'textarea' ];
+		$tagName = strtolower( $tag->getTag() );
+
+		if ( $tagName === 'input' && $tag->getAttribute( 'type' ) !== 'hidden' ) {
+			return true;
+		}
+		if ( in_array( $tagName, $labelableTags, true ) ) {
+			return true;
+		}
+		return false;
+	}
 }

php/widgets/ButtonInputWidget.php
@@ -15,12 +15,6 @@ class ButtonInputWidget extends InputWidget {
 
 	/* Static Properties */
 
-	/**
-	 * Disable generating `<label>` elements for buttons. One would very rarely need additional label
-	 * for a button, and it's already a big clickable target, and it causes unexpected rendering.
-	 */
-	public static $supportsSimpleLabel = false;
-
 	public static $tagName = 'span';
 
 	/* Properties */
@@ -111,6 +105,12 @@ class ButtonInputWidget extends InputWidget {
 			parent::setValue( $value );
 		}
 		return $this;
+	}
+
+	public function getInputId() {
+		// Disable generating `<label>` elements for buttons. One would very rarely need additional label
+		// for a button, and it's already a big clickable target, and it causes unexpected rendering.
+		return null;
 	}
 
 	public function getConfig( &$config ) {

php/widgets/CheckboxMultiselectInputWidget.php
@@ -7,10 +7,6 @@ namespace OOUI;
  */
 class CheckboxMultiselectInputWidget extends InputWidget {
 
-	/* Static Properties */
-
-	public static $supportsSimpleLabel = false;
-
 	/* Properties */
 
 	/**

php/widgets/InputWidget.php
@@ -13,10 +13,6 @@ class InputWidget extends Widget {
 	use TitledElement;
 	use AccessKeyedElement;
 
-	/* Static Properties */
-
-	public static $supportsSimpleLabel = true;
-
 	/* Properties */
 
 	/**
@@ -80,25 +76,6 @@ class InputWidget extends Widget {
 	 */
 	protected function getInputElement( $config ) {
 		return new Tag( 'input' );
-	}
-
-	/**
-	 * Get input element's ID.
-	 *
-	 * If the element already has an ID then that is returned, otherwise unique ID is
-	 * generated, set on the element, and returned.
-	 *
-	 * @return {string} The ID of the element
-	 */
-	public function getInputId() {
-		$id = $this->input->getAttribute( 'id' );
-
-		if ( $id === null ) {
-			$id = Tag::generateElementId();
-			$this->input->setAttributes( [ 'id' => $id ] );
-		}
-
-		return $id;
 	}
 
 	/**

php/widgets/LabelWidget.php
@@ -37,7 +37,7 @@ class LabelWidget extends Widget {
 		$this->input = isset( $config['input'] ) ? $config['input'] : null;
 
 		// Initialization
-		if ( $this->input instanceof InputWidget ) {
+		if ( $this->input && $this->input->getInputId() ) {
 			$this->setAttributes( [ 'for' => $this->input->getInputId() ] );
 		}
 		$this->addClasses( [ 'oo-ui-labelWidget' ] );

php/widgets/RadioSelectInputWidget.php
@@ -7,10 +7,6 @@ namespace OOUI;
  */
 class RadioSelectInputWidget extends InputWidget {
 
-	/* Static Properties */
-
-	public static $supportsSimpleLabel = false;
-
 	/* Properties */
 
 	/**

src/Widget.js
@@ -37,19 +37,6 @@ OO.ui.Widget = function OoUiWidget( config ) {
 OO.inheritClass( OO.ui.Widget, OO.ui.Element );
 OO.mixinClass( OO.ui.Widget, OO.EventEmitter );
 
-/* Static Properties */
-
-/**
- * Whether this widget will behave reasonably when wrapped in an HTML `<label>`. If this is true,
- * wrappers such as OO.ui.FieldLayout may use a `<label>` instead of implementing own label click
- * handling.
- *
- * @static
- * @inheritable
- * @property {boolean}
- */
-OO.ui.Widget.static.supportsSimpleLabel = false;
-
 /* Events */
 
 /**
@@ -113,4 +100,14 @@ OO.ui.Widget.prototype.setDisabled = function ( disabled ) {
 OO.ui.Widget.prototype.updateDisabled = function () {
 	this.setDisabled( this.disabled );
 	return this;
+};
+
+/**
+ * Get an ID of a labelable node which is part of this widget, if any, to be used for `<label for>`
+ * value.
+ *
+ * @return {string|null} The ID of the labelable element
+ */
+OO.ui.Widget.prototype.getInputId = function () {
+	return null;
 };

src/layouts/FieldLayout.js
@@ -97,7 +97,7 @@ OO.ui.FieldLayout = function OoUiFieldLayout( fieldWidget, config ) {
 	this.fieldWidget.connect( this, { disable: 'onFieldDisable' } );
 
 	// Initialization
-	if ( fieldWidget.constructor.static.supportsSimpleLabel && this.fieldWidget.getInputId() ) {
+	if ( this.fieldWidget.getInputId() ) {
 		this.$label.attr( 'for', this.fieldWidget.getInputId() );
 	} else {
 		this.$label.on( 'click', function () {

src/mixins/TabIndexedElement.js
@@ -139,6 +139,56 @@ OO.ui.mixin.TabIndexedElement.prototype.getTabIndex = function () {
 };
 
 /**
+ * Get an ID of a focusable element of this widget, if any, to be used for `<label for>` value.
+ *
+ * If the element already has an ID then that is returned, otherwise unique ID is
+ * generated, set on the element, and returned.
+ *
+ * @return {string|null} The ID of the focusable element
+ */
+OO.ui.mixin.TabIndexedElement.prototype.getInputId = function () {
+	var id;
+
+	if ( !this.$tabIndexed ) {
+		return null;
+	}
+	if ( !this.isLabelableNode( this.$tabIndexed ) ) {
+		return null;
+	}
+
+	id = this.$tabIndexed.attr( 'id' );
+	if ( id === undefined ) {
+		id = OO.ui.generateElementId();
+		this.$tabIndexed.attr( 'id', id );
+	}
+
+	return id;
+};
+
+/**
+ * Whether the node is 'labelable' according to the HTML spec
+ * (i.e., whether it can be interacted with through a `<label for="…">`).
+ * See: <https://html.spec.whatwg.org/multipage/forms.html#category-label>.
+ *
+ * @private
+ * @param {jQuery} $node
+ * @return {boolean}
+ */
+OO.ui.mixin.TabIndexedElement.prototype.isLabelableNode = function ( $node ) {
+	var
+		labelableTags = [ 'button', 'meter', 'output', 'progress', 'select', 'textarea' ],
+		tagName = $node.prop( 'tagName' ).toLowerCase();
+
+	if ( tagName === 'input' && $node.attr( 'type' ) !== 'hidden' ) {
+		return true;
+	}
+	if ( labelableTags.indexOf( tagName ) !== -1 ) {
+		return true;
+	}
+	return false;
+};
+
+/**
  * Focus this element.
  *
  * @chainable

src/widgets/ButtonInputWidget.js
@@ -73,15 +73,6 @@ OO.mixinClass( OO.ui.ButtonInputWidget, OO.ui.mixin.TitledElement );
 /* Static Properties */
 
 /**
- * Disable generating `<label>` elements for buttons. One would very rarely need additional label
- * for a button, and it's already a big clickable target, and it causes unexpected rendering.
- *
- * @static
- * @inheritdoc
- */
-OO.ui.ButtonInputWidget.static.supportsSimpleLabel = false;
-
-/**
  * @static
  * @inheritdoc
  */
@@ -139,4 +130,13 @@ OO.ui.ButtonInputWidget.prototype.setValue = function ( value ) {
 		OO.ui.ButtonInputWidget.parent.prototype.setValue.call( this, value );
 	}
 	return this;
+};
+
+/**
+ * @inheritdoc
+ */
+OO.ui.ButtonInputWidget.prototype.getInputId = function () {
+	// Disable generating `<label>` elements for buttons. One would very rarely need additional label
+	// for a button, and it's already a big clickable target, and it causes unexpected rendering.
+	return null;
 };

src/widgets/CapsuleMultiselectWidget.js
@@ -204,10 +204,6 @@ OO.mixinClass( OO.ui.CapsuleMultiselectWidget, OO.ui.mixin.TabIndexedElement );
 OO.mixinClass( OO.ui.CapsuleMultiselectWidget, OO.ui.mixin.IndicatorElement );
 OO.mixinClass( OO.ui.CapsuleMultiselectWidget, OO.ui.mixin.IconElement );
 
-/* Static Properties */
-
-OO.ui.CapsuleMultiselectWidget.static.supportsSimpleLabel = true;
-
 /* Events */
 
 /**
@@ -244,23 +240,13 @@ OO.ui.CapsuleMultiselectWidget.prototype.createItemWidget = function ( data, lab
 };
 
 /**
- * Get the widget's input's id, or generate one, if it has an input.
- *
- * @return {string}
+ * @inheritdoc
  */
 OO.ui.CapsuleMultiselectWidget.prototype.getInputId = function () {
-	var id;
 	if ( !this.$input ) {
-		return false;
+		return null;
 	}
-
-	id = this.$input.attr( 'id' );
-	if ( id === undefined ) {
-		id = OO.ui.generateElementId();
-		this.$input.attr( 'id', id );
-	}
-
-	return id;
+	return OO.ui.mixin.TabIndexedElement.prototype.getInputId.call( this );
 };
 
 /**

src/widgets/CheckboxMultiselectInputWidget.js
@@ -53,14 +53,6 @@ OO.ui.CheckboxMultiselectInputWidget = function OoUiCheckboxMultiselectInputWidg
 
 OO.inheritClass( OO.ui.CheckboxMultiselectInputWidget, OO.ui.InputWidget );
 
-/* Static Properties */
-
-/**
- * @static
- * @inheritdoc
- */
-OO.ui.CheckboxMultiselectInputWidget.static.supportsSimpleLabel = false;
-
 /* Static Methods */
 
 /**

src/widgets/InputWidget.js
@@ -66,14 +66,6 @@ OO.mixinClass( OO.ui.InputWidget, OO.ui.mixin.TabIndexedElement );
 OO.mixinClass( OO.ui.InputWidget, OO.ui.mixin.TitledElement );
 OO.mixinClass( OO.ui.InputWidget, OO.ui.mixin.AccessKeyedElement );
 
-/* Static Properties */
-
-/**
- * @static
- * @inheritdoc
- */
-OO.ui.InputWidget.static.supportsSimpleLabel = true;
-
 /* Static Methods */
 
 /**
@@ -123,25 +115,6 @@ OO.ui.InputWidget.static.gatherPreInfuseState = function ( node, config ) {
  */
 OO.ui.InputWidget.prototype.getInputElement = function () {
 	return $( '<input>' );
-};
-
-/**
- * Get input element's ID.
- *
- * If the element already has an ID then that is returned, otherwise unique ID is
- * generated, set on the element, and returned.
- *
- * @return {string} The ID of the element
- */
-OO.ui.InputWidget.prototype.getInputId = function () {
-	var id = this.$input.attr( 'id' );
-
-	if ( id === undefined ) {
-		id = OO.ui.generateElementId();
-		this.$input.attr( 'id', id );
-	}
-
-	return id;
 };
 
 /**

src/widgets/LabelWidget.js
@@ -53,7 +53,7 @@ OO.ui.LabelWidget = function OoUiLabelWidget( config ) {
 
 	// Initialization
 	if ( this.input ) {
-		if ( this.input instanceof OO.ui.InputWidget && this.input.getInputId() ) {
+		if ( this.input.getInputId() ) {
 			this.$element.attr( 'for', this.input.getInputId() );
 		} else {
 			this.$label.on( 'click', function () {

src/widgets/RadioSelectInputWidget.js
@@ -51,14 +51,6 @@ OO.ui.RadioSelectInputWidget = function OoUiRadioSelectInputWidget( config ) {
 
 OO.inheritClass( OO.ui.RadioSelectInputWidget, OO.ui.InputWidget );
 
-/* Static Properties */
-
-/**
- * @static
- * @inheritdoc
- */
-OO.ui.RadioSelectInputWidget.static.supportsSimpleLabel = false;
-
 /* Static Methods */
 
 /**