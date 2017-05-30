4886ec9869ecf81e39b3220819bf3950a6b34255
Volker E <volker.e@wikimedia.org>
2017-05-30 16:02:19 +0200

    WikimediaUI theme: Use icon instead of indicator in Tag-/CapsuleItemWidget
    Use close icon as requested by design and clean-up Less structure.
    
    Bug: T159208
    Bug: T158400
    Change-Id: I3c3c9751d618d36e3a556ef55cff35a84429719b

--- src/widgets/CapsuleItemWidget.js
+++ src/widgets/CapsuleItemWidget.js
@@ -28,7 +28,7 @@ OO.ui.CapsuleItemWidget = function OoUiCapsuleItemWidget( config ) {
 	// Events
 	this.closeButton = new OO.ui.ButtonWidget( {
 		framed: false,
-		indicator: 'clear',
+		icon: 'close',
 		tabIndex: -1
 	} ).on( 'click', this.onCloseClick.bind( this ) );
 
