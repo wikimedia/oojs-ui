1e0e316a62014d156a548cc6a4e890fdf064e134
Bartosz Dziewoński <matma.rex@gmail.com>
2017-05-31 14:01:24 +0200

    WindowManager: Fix check for a window already closing
    This was not updated in ac10e40b6b4ab740c113563ea5161fe94990e451.
    
    Bug: T166631
    Change-Id: I432f3919e61443a17dadfb018c6c319a165dcfc0

--- src/WindowManager.js
+++ src/WindowManager.js
@@ -471,7 +471,7 @@ OO.ui.WindowManager.prototype.closeWindow = function ( win, data ) {
 		error = 'Cannot close window: window is not attached to manager';
 	} else if ( win !== this.currentWindow ) {
 		error = 'Cannot close window: window already closed with different data';
-	} else if ( this.preparingToClose || this.closing ) {
+	} else if ( this.preparingToClose || lifecycle.closing.state() === 'resolved' ) {
 		error = 'Cannot close window: window already closing with different data';
 	}
 
