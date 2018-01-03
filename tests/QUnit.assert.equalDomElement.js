aefe47fe8f8ea5a1be530ccbe12bd918e28e7133
James D. Forrester <jforrester@wikimedia.org>
2018-01-02 16:56:58 -0800

    doc: Bump copyright year
    Change-Id: Ief9d2159c137ee73c9a305977f720fae4bb787ae

--- tests/QUnit.assert.equalDomElement.js
+++ tests/QUnit.assert.equalDomElement.js
@@ -5,8 +5,7 @@
  * attributes (for dynamically generated nodes) and order-insensitive comparison of classes on DOM
  * nodes.
  *
- * @copyright 2011-2017 VisualEditor Team and others; see http://ve.mit-license.org
- * @copyright 2011-2017 OOjs UI Team and other contributors
+ * @copyright 2011-2018 VisualEditor Team and others; see http://ve.mit-license.org
  */
 
 ( function ( QUnit ) {
