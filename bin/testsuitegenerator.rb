d1b8e8ecc1eacc7816124ea4ccba4fc80cb5ef3c
Bartosz Dziewoński <matma.rex@gmail.com>
2019-03-13 23:22:49 +0100

    testsuitegenerator: Use normal methods more instead of lambdas
    I am not sure why I wrote it this way, but if I had to guess:
    
    * Using lambdas allowed for the fancy syntax with currying and with
      passing the lambda as a block to another method
    * Lambdas inherited the scope, so we did not have to pass around a few
      of the variables, they were kind of all global
    
    Now older and wiser, I think the fancy syntax is often a bad idea
    (especially in a repository where Ruby code is the exception), and
    treating all variables as global is almost always a bad idea.
    
    Change-Id: Icea57bdb8397e128048cc13e9cd2ed7f166d726b

--- bin/testsuitegenerator.rb
+++ bin/testsuitegenerator.rb
@@ -1,6 +1,83 @@
 require 'pp'
 require_relative 'docparser'
 
+def make_class_instance_placeholder klass, config
+	'_placeholder_' + {
+		class: klass,
+		config: config
+	}.to_json
+end
+
+def find_class classes, klass
+	classes.find{|c| c[:name] == klass }
+end
+
+def expand_types_to_values classes, types
+	# values to test for each type
+	expandos = {
+		'null' => [nil],
+		'int' => [0, -1, 300], # PHP code
+		'number' => [0, -1, 300], # JS code
+		'bool' => [true, false], # PHP code
+		'boolean' => [true, false], # JS code
+		'string' => ['Foo bar', '<b>HTML?</b>', '', ' ', '0'],
+		'HtmlSnippet' => ['Foo bar', '<b>HTML?</b>', ''].map{|v|
+			make_class_instance_placeholder('HtmlSnippet', v)
+		},
+		'PanelLayout' => ['Foo bar', '<b>HTML?</b>', ''].map{|v|
+			config = { content: [v] }
+			make_class_instance_placeholder('PanelLayout', config)
+		},
+	}
+
+	# For abstract classes (not "testable"), test a few different subclasses instead
+	if types.delete 'Widget'
+		types.push 'ButtonWidget', 'TextInputWidget'
+	end
+	if types.delete 'InputWidget'
+		types.push 'CheckboxInputWidget', 'TextInputWidget'
+	end
+
+	types.map{|t|
+		as_array = true if t.sub! '[]', ''
+		if expandos[t]
+			# Primitive. Run tests with the provided values.
+			vals = expandos[t]
+		elsif find_class(classes, t)
+			# OOUI object. Test suite will instantiate one and run the test with it.
+			constructor = find_class(classes, t)[:methods].find{|m| m[:name] == '#constructor' }
+			params = constructor ? (constructor[:params] || []) : []
+			config = params.map{|config_option|
+				types = config_option[:type].split '|'
+				values = expand_types_to_values(classes, types)
+				{ config_option[:name] => values[0] }
+			}
+			vals = [
+				make_class_instance_placeholder( t, config.inject({}, :merge) )
+			]
+		else
+			# We don't know how to test this. The empty value will result in no
+			# tests being generated for this combination of config values.
+			vals = []
+		end
+		as_array ? vals.map{|v| [v] } : vals
+	}.inject(:+)
+end
+
+def find_config_sources classes, klass_name
+	return [] unless klass_name
+	klass_names = [klass_name]
+	while klass_name
+		klass = find_class(classes, klass_name)
+		break unless klass
+		klass_names +=
+			find_config_sources(classes, klass[:parent]) +
+			klass[:mixins].map{|mixin_name| find_config_sources(classes, mixin_name) }.flatten
+		klass_name = klass[:parent]
+	end
+	klass_names.uniq
+end
+
 if ARGV.empty? || ARGV == ['-h'] || ARGV == ['--help']
 	$stderr.puts "usage: ruby #{$PROGRAM_NAME} <dirA> <dirB> -o <file>"
 	$stderr.puts "       ruby #{$PROGRAM_NAME} src php -o tests/JSPHP-suite.json"
@@ -30,29 +107,8 @@ else
 		.reject{|c| !c[:parent] || c[:trait] || c[:parent] == 'Theme' } # can't test abstract
 		.reject{|c| %w[Element Widget Layout Theme].include? c[:name] } # no toplevel
 
-	make_class_instance_placeholder = lambda do |klass, config|
-		'_placeholder_' + {
-			class: klass,
-			config: config
-		}.to_json
-	end
-
-	make_htmlsnippet_placeholder = make_class_instance_placeholder.curry['HtmlSnippet']
-	make_panellayout_placeholder = make_class_instance_placeholder.curry['PanelLayout']
-
-	# values to test for each type
-	expandos = {
-		'null' => [nil],
-		'int' => [0, -1, 300], # PHP code
-		'number' => [0, -1, 300], # JS code
-		'bool' => [true, false], # PHP code
-		'boolean' => [true, false], # JS code
-		'string' => ['Foo bar', '<b>HTML?</b>', '', ' ', '0'],
-		'HtmlSnippet' => ['Foo bar', '<b>HTML?</b>', ''].map(&make_htmlsnippet_placeholder),
-		'PanelLayout' => ['Foo bar', '<b>HTML?</b>', ''].map{|v| {content: [v]} }.map(&make_panellayout_placeholder),
-	}
-
-	string_list = expandos['string'].map{|v| [v] }
+	# string_list = expandos['string'].map{|v| [v] }
+	string_list = ['Foo bar', '<b>HTML?</b>', '', ' ', '0'].map{|v| [v] }
 
 	# Values to test for specific config options, when not all values of given type are valid.
 	# Empty array will result in no tests for this config option being generated.
@@ -120,59 +176,6 @@ else
 		['IndexLayout', 'menuPanel'] => [],
 	}
 
-	find_class = lambda do |klass|
-		return classes.find{|c| c[:name] == klass }
-	end
-
-	expand_types_to_values = lambda do |types|
-		# For abstract classes (not "testable"), test a few different subclasses instead
-		if types.delete 'Widget'
-			types.push 'ButtonWidget', 'TextInputWidget'
-		end
-		if types.delete 'InputWidget'
-			types.push 'CheckboxInputWidget', 'TextInputWidget'
-		end
-
-		return types.map{|t|
-			as_array = true if t.sub! '[]', ''
-			if expandos[t]
-				# Primitive. Run tests with the provided values.
-				vals = expandos[t]
-			elsif testable_classes.find{|c| c[:name] == t }
-				# OOUI object. Test suite will instantiate one and run the test with it.
-				constructor = find_class.call(t)[:methods].find{|m| m[:name] == '#constructor' }
-				params = constructor ? (constructor[:params] || []) : []
-				config = params.map{|config_option|
-					types = config_option[:type].split '|'
-					values = expand_types_to_values.call(types)
-					{ config_option[:name] => values[0] }
-				}
-				vals = [
-					make_class_instance_placeholder.call( t, config.inject({}, :merge) )
-				]
-			else
-				# We don't know how to test this. The empty value will result in no
-				# tests being generated for this combination of config values.
-				vals = []
-			end
-			as_array ? vals.map{|v| [v] } : vals
-		}.inject(:+)
-	end
-
-	find_config_sources = lambda do |klass_name|
-		return [] unless klass_name
-		klass_names = [klass_name]
-		while klass_name
-			klass = find_class.call(klass_name)
-			break unless klass
-			klass_names +=
-				find_config_sources.call(klass[:parent]) +
-				klass[:mixins].map(&find_config_sources).flatten
-			klass_name = klass[:parent]
-		end
-		return klass_names.uniq
-	end
-
 	testable_classes.each do |klass|
 		class_name = klass[:name]
 		tests[class_name] = {
@@ -180,8 +183,8 @@ else
 			tests: [],
 		}
 
-		config_sources = find_config_sources.call(class_name)
-			.map{|c| find_class.call(c)[:methods].find{|m| m[:name] == '#constructor' } }
+		config_sources = find_config_sources(classes, class_name)
+			.map{|c| find_class(classes, c)[:methods].find{|m| m[:name] == '#constructor' } }
 		config = config_sources.compact.map{|c| c[:config] }.compact.inject([], :+)
 		constructor = klass[:methods].find{|m| m[:name] == '#constructor' }
 		required_config = constructor ? (constructor[:params] || []) : []
@@ -197,7 +200,7 @@ else
 				values =
 					sensible_values[ [ class_name, config_option[:name] ] ] ||
 					sensible_values[ config_option[:name] ] ||
-					expand_types_to_values.call(types)
+					expand_types_to_values(testable_classes, types)
 				values.map{|v| config_option.dup.merge(value: v) }
 			}
 			expanded.empty? ? [ [] ] : expanded[0].product(*expanded[1..-1])
