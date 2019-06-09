1849095b9f8dec1e6343fd8f4c2e0c5c0f60ee23
Ed Sanders <esanders@wikimedia.org>
2019-06-09 15:11:58 -0400

    Scroll tab to centre on mobile
    Change-Id: Ia0640e2fd9daa59bd2866d4b92106c0ccaeb82a8

--- src/widgets/TabOptionWidget.js
+++ src/widgets/TabOptionWidget.js
@@ -35,3 +35,37 @@ OO.inheritClass( OO.ui.TabOptionWidget, OO.ui.OptionWidget );
  * @inheritdoc
  */
 OO.ui.TabOptionWidget.static.highlightable = false;
+
+/**
+ * @static
+ * @inheritdoc
+ */
+OO.ui.TabOptionWidget.static.scrollIntoViewOnSelect = true;
+
+/**
+ * Center tab horizontally after selecting on mobile
+ *
+ * @param {Object} [config] Configuration options
+ * @return {jQuery.Promise} Promise which resolves when the scroll is complete
+ */
+OO.ui.TabOptionWidget.prototype.scrollElementIntoView = function ( config ) {
+	var padding;
+	if ( !OO.ui.isMobile() || !this.getElementGroup() ) {
+		// Parent method
+		return OO.ui.TabOptionWidget.super.prototype.scrollElementIntoView.call( this );
+	} else {
+		padding = Math.max( (
+			this.getElementGroup().$element[ 0 ].clientWidth - this.$element[ 0 ].clientWidth
+		) / 2, 0 );
+		// Parent method
+		return OO.ui.TabOptionWidget.super.prototype.scrollElementIntoView.call( this, $.extend(
+			{
+				padding: {
+					left: padding,
+					right: padding
+				}
+			},
+			config
+		) );
+	}
+};
