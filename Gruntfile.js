223498c4a6be8378cd9fd0593946f718c3a36c1e
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2020-01-16 15:44:48 +0100

    version bump and turned off themes other than fandom

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,9 @@ module.exports = function ( grunt ) {
 	var modules = grunt.file.readYAML( 'build/modules.yaml' ),
 		pkg = grunt.file.readJSON( 'package.json' ),
 		themes = {
-			fandom: 'Fandom',
-			wikimediaui: 'WikimediaUI', // Do not change this line or you'll break `grunt add-theme`
-			apex: 'Apex'
+			fandom: 'Fandom'
+			// wikimediaui: 'WikimediaUI', // Do not change this line or you'll break `grunt add-theme`
+			// apex: 'Apex'
 		},
 		lessFiles = {},
 		lessTargets = {},
