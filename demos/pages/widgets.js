8e13d638d453f4934254c38b123247f3d1dd98b0
Ed Sanders <esanders@wikimedia.org>
2020-04-10 16:53:16 +0100

    Implement ButtonMenuSelectWidget
    Very similar to ButtonPopupWidget except launches a
    MenuSelectWidget.
    
    When the menu is open the button holds its pressed state.
    
    By default when an item is selected it is immediately cleared.
    
    Bug: T178387
    Change-Id: I5d8fec6d06f7f57d4b017dad756842c640f55210

--- demos/pages/widgets.js
+++ demos/pages/widgets.js
@@ -2169,6 +2169,122 @@ Demo.static.pages.widgets = function ( demo ) {
 			]
 		} ),
 		new Demo.LinkedFieldsetLayout( {
+			id: 'demo-section-buttonMenuSelect',
+			label: 'ButtonMenuSelect',
+			items: [
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonMenuSelectWidget( {
+						icon: 'menu',
+						label: 'Options',
+						menu: {
+							items: [
+								new OO.ui.MenuOptionWidget( {
+									data: 'a',
+									label: 'First'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'b',
+									label: 'Second',
+									indicator: 'clear'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'Third'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'The fourth option has an overly long label'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									icon: 'feedback',
+									data: 'd',
+									label: 'The fifth option has an icon'
+								} )
+							]
+						}
+					} ),
+					{
+						label: 'ButtonMenuSelectWidget',
+						align: 'top'
+					}
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonMenuSelectWidget( {
+						icon: 'menu',
+						label: 'Options',
+						clearOnSelect: false,
+						menu: {
+							items: [
+								new OO.ui.MenuOptionWidget( {
+									data: 'a',
+									label: 'First'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'b',
+									label: 'Second',
+									indicator: 'clear'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'Third'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'The fourth option has an overly long label'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									icon: 'feedback',
+									data: 'd',
+									label: 'The fifth option has an icon'
+								} )
+							]
+						}
+					} ),
+					{
+						label: 'ButtonMenuSelectWidget (clearOnSelect=false)',
+						align: 'top'
+					}
+				),
+				new OO.ui.FieldLayout(
+					new OO.ui.ButtonMenuSelectWidget( {
+						icon: 'ellipsis',
+						framed: false,
+						menu: {
+							horizontalPosition: 'end',
+							items: [
+								new OO.ui.MenuOptionWidget( {
+									data: 'a',
+									label: 'First'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'b',
+									label: 'Second',
+									indicator: 'clear'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'Third'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									data: 'c',
+									label: 'The fourth option has an overly long label'
+								} ),
+								new OO.ui.MenuOptionWidget( {
+									icon: 'feedback',
+									data: 'd',
+									label: 'The fifth option has an icon'
+								} )
+							]
+						}
+					} ),
+					{
+						label: 'ButtonMenuSelectWidget (quiet, no label, horizontalPosition="end")',
+						align: 'top'
+					}
+				)
+			]
+		} ),
+		new Demo.LinkedFieldsetLayout( {
 			id: 'demo-section-popupButton',
 			label: 'PopupButton',
 			items: [
---
+++ src/widgets/ButtonMenuSelectWidget.js
@@ -0,0 +1,133 @@
+/**
+ * ButtonMenuSelectWidgets launch a menu of options created with OO.ui.MenuOptionWidget.
+ * The ButtonMenuSelectWidget takes care of opening and displaying the menu so that
+ * users can interact with it.
+ *
+ *     @example
+ *     // A ButtonMenuSelectWidget with a menu that contains three options.
+ *     var buttonMenu = new OO.ui.ButtonMenuSelectWidget( {
+ *         icon: 'menu',
+ *         menu: {
+ *             items: [
+ *                 new OO.ui.MenuOptionWidget( {
+ *                     data: 'a',
+ *                     label: 'First'
+ *                 } ),
+ *                 new OO.ui.MenuOptionWidget( {
+ *                     data: 'b',
+ *                     label: 'Second'
+ *                 } ),
+ *                 new OO.ui.MenuOptionWidget( {
+ *                     data: 'c',
+ *                     label: 'Third'
+ *                 } )
+ *             ]
+ *         }
+ *     } );
+ *
+ *     $( document.body ).append( buttonMenu.$element );
+ *
+ *     // When using the `clearOnSelect` option, listen to the `choose` event
+ *     // to avoid getting the null select event.
+ *     buttonMenu.getMenu().on( 'choose', function ( menuOption ) {
+ *         console.log( menuOption.getData() );
+ *     } );
+ *
+ * @class
+ * @extends OO.ui.ButtonWidget
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {boolean} [clearOnSelect=true] Clear selection immediately after making it
+ * @cfg {Object} [menu] Configuration options to pass to
+ *  {@link OO.ui.MenuSelectWidget menu select widget}.
+ * @cfg {jQuery|boolean} [$overlay] Render the menu into a separate layer. This configuration is
+ *  useful in cases where the expanded menu is larger than its containing `<div>`. The specified
+ *  overlay layer is usually on top of the containing `<div>` and has a larger area. By default,
+ *  the menu uses relative positioning. Pass 'true' to use the default overlay.
+ *  See <https://www.mediawiki.org/wiki/OOUI/Concepts#Overlays>.
+ */
+OO.ui.ButtonMenuSelectWidget = function OoUiButtonMenuSelectWidget( config ) {
+	// Configuration initialization
+	config = config || {};
+
+	// Parent constructor
+	OO.ui.ButtonMenuSelectWidget.parent.call( this, config );
+
+	this.$overlay = ( config.$overlay === true ?
+		OO.ui.getDefaultOverlay() : config.$overlay ) || this.$element;
+
+	// Properties
+	this.clearOnSelect = config.clearOnSelect !== false;
+	this.menu = new OO.ui.MenuSelectWidget( $.extend( {
+		widget: this,
+		$floatableContainer: this.$element
+	}, config.menu ) );
+
+	// Events
+	this.connect( this, {
+		click: 'onButtonMenuClick'
+	} );
+	this.getMenu().connect( this, {
+		select: 'onMenuSelect',
+		toggle: 'onMenuToggle'
+	} );
+
+	// Initialization
+	this.$button
+		.attr( {
+			'aria-expanded': 'false',
+			'aria-haspopup': 'true',
+			'aria-owns': this.menu.getElementId()
+		} );
+	this.$element.addClass( 'oo-ui-buttonMenuSelectWidget' );
+	this.$overlay.append( this.menu.$element );
+};
+
+/* Setup */
+
+OO.inheritClass( OO.ui.ButtonMenuSelectWidget, OO.ui.ButtonWidget );
+
+/* Methods */
+
+/**
+ * Get the menu.
+ *
+ * @return {OO.ui.MenuSelectWidget} Menu of widget
+ */
+OO.ui.ButtonMenuSelectWidget.prototype.getMenu = function () {
+	return this.menu;
+};
+
+/**
+ * Handle menu select events.
+ *
+ * @private
+ * @param {OO.ui.MenuOptionWidget} item Selected menu item
+ */
+OO.ui.ButtonMenuSelectWidget.prototype.onMenuSelect = function ( item ) {
+	if ( this.clearOnSelect && item ) {
+		// This will cause an additional 'select' event to fire, so
+		// users should probably listen to the 'choose' event.
+		this.getMenu().selectItem();
+	}
+};
+
+/**
+ * Handle menu toggle events.
+ *
+ * @private
+ * @param {boolean} isVisible Open state of the menu
+ */
+OO.ui.ButtonMenuSelectWidget.prototype.onMenuToggle = function ( isVisible ) {
+	this.$element.toggleClass( 'oo-ui-buttonElement-pressed', isVisible );
+};
+
+/**
+ * Handle mouse click events.
+ *
+ * @private
+ */
+OO.ui.ButtonMenuSelectWidget.prototype.onButtonMenuClick = function () {
+	this.menu.toggle();
+};
