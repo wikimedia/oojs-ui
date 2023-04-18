47819c7d2696c7b6172db58f048d33a0f01694e2
Umherirrender <umherirrender_de.wp@web.de>
2023-04-18 21:19:07 +0200

    build: Clean up of phan config
    MW_VENDOR_PATH is not used by config-library.php
    
    Follow-Up: I80177acf3219540a5421af475ef9bc0c188031fd
    Change-Id: Id4c2e9af00963069f1d6cc9345fc675d15e7f777

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,5 @@
 <?php
 
-// Always use repo's own vendor folder
-putenv( 'MW_VENDOR_PATH=.' );
-
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 $cfg['directory_list'][] = 'php';
